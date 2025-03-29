6e636245b48db9c75d79a91bd49b0f8faddec68f
anshikag31 <105929985+anshikag31@users.noreply.github.com>
2025-03-30 04:30:12 +0530

    Delete disease_prediction directory

--- disease_prediction/predictor/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import predict_disease  # Ensure you import the correct function from views.py
-
-urlpatterns = [
-    path('predict/', predict_disease, name='predict'),
-]
